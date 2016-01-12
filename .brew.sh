5c1835c78b8d2b80979abb29b1580a002e598a0a
Daniel Furze <daniel@furzeface.com>
2016-01-12 17:06:17 +0000

    feat: add Node version 0.12 and NVM to brew script

--- .brew.sh
+++ .brew.sh
@@ -12,14 +12,24 @@ echo Installing Homebrew \(gets Xcode cli developer tools too\)…
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo xcodebuild -license
 
+# Tap versions for alternate versions of applications
+brew tap homebrew/versions
+
+
 # Install Git
 echo Installing Git…
 brew install git
-# brew install git-flow
+
 
 # Install Node.js - this installs `npm` too, using the recommended installation method
 echo Installing Node…
-brew install node
+# brew install node  Uncomment for latest
+brew install node012
+
+# Install Node Version Manager
+brew install nvm
+echo "source $(brew --prefix nvm)/nvm.sh" >> ~/.bashrc
+
 
 # Install Brew Cask for managing other apps
 echo Installing Brew Cask…
