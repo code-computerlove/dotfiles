842a4d608f5f8fd318b8e5c2c55836b272825bf1
Daniel Furze <daniel@furzeface.com>
2016-01-12 09:56:05 +0000

    add installations to brew

--- .brew.sh
+++ .brew.sh
@@ -2,5 +2,27 @@
 #
 # ~/.brew.sh
 #
+# This script installs Homebrew and uses it to install other command line tools
 #
 #
+
+# Install Homebrew
+echo Installing Homebrew \(gets Xcode cli developer tools too\)…
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo xcodebuild -license
+
+# Install Git
+echo Installing Git…
+brew install git
+# brew install git-flow
+
+# Install Node.js - this installs `npm` too, using the recommended installation method
+echo Installing Node…
+brew install node
+
+# Install Brew Cask for managing other apps
+echo Installing Brew Cask…
+brew install caskroom/cask/brew-cask
+
+# Install Cask versions for beta version of apps
+brew tap caskroom/versions
